d5cae0151d3873bf68331826e1c872b7afd99f5c
iamaprin <i@zhukuanxin.cn>
2017-07-06 00:18:47 +0800

    add mem-popup plugin
    init

---
+++ mem-popup/config.js
@@ -0,0 +1,9 @@
+var require = {
+	baseUrl: './',
+	shim: {
+
+	},
+	paths: {
+		'jquery': 'lib/jquery-3.2.1'
+	}
+};
---
+++ mem-popup/dist/index.js
@@ -0,0 +1,8 @@
+define(["require", "exports", "jquery"], function (require, exports, $) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    $(function () {
+        // 
+    });
+});
+//# sourceMappingURL=index.js.map
