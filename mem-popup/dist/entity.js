1ff75b4fd234b084c45a95e31d3e84db46709834
iamaprin <i@zhukuanxin.cn>
2017-09-17 01:43:20 +0800

    delete 'dist' directory

--- mem-popup/dist/entity.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author iamaprin
- */
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-});
-//# sourceMappingURL=entity.js.map
--- mem-popup/dist/index.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", "jquery", "./mem-popup"], function (require, exports, $, mem_popup_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    $(function () {
-        let memPopup = new mem_popup_1.MemPopup();
-        memPopup.init();
-    });
-});
-//# sourceMappingURL=index.js.map
